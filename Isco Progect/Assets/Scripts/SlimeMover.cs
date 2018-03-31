531c18e28e88f06186683f194d4fb56cb36abdb6
JoshuaDWalters <37957669+JoshuaDWalters@users.noreply.github.com>
2018-03-31 15:12:22 +0100

    FFDDGGA

---
+++ Isco Progect/Assets/Scripts/SlimeMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class SlimeMover : MonoBehaviour {
+
+	NavMeshAgent agent;
+	public Transform food;
+	public float speed1;
+	public float speed2;
+
+	void Start ()
+	{
+		agent = this.GetComponent<NavMeshAgent> ();
+		agent.speed = Random.Range (speed1 * 1.0f, speed2 * 1.0f);
+	}
+
+	void Update ()
+	{
+		agent.SetDestination (food.position);
+		if (agent.destination == (food.position))
+			Destroy (food);
+	
+		}
+}
---
+++ Isco Progect/Assets/Scripts/SlimePickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimePickup : MonoBehaviour {
+	Vector3 dist;
+	float posX;
+	float posY;
+
+	// Use this for initialization
+	void OnMouseDown() {
+		dist = Camera.main.WorldToScreenPoint (transform.position);
+		posX = Input.mousePosition.x - dist.x;
+		posY = Input.mousePosition.y - dist.y;
+
+		
+	}
+	
+	// Update is called once per frame
+	void OnMouseDrag () {
+		Vector3 curPos = new Vector3 (Input.mousePosition.x - posX, Input.mousePosition.y - posY, dist.z);
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+			transform.position = worldPos;
+		
+	}
+}
