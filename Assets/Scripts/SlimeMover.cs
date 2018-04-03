1728d50e3e5dd57d7cffb119f1da792920a62fb0
JoshuaDWalters <37957669+JoshuaDWalters@users.noreply.github.com>
2018-04-03 16:00:17 +0100

    Gem Spawning
    Gem Spawning

--- Assets/Scripts/SlimeMover.cs
+++ Assets/Scripts/SlimeMover.cs
@@ -8,6 +8,7 @@ public class SlimeMover : MonoBehaviour {
 	public Transform food;
 	public float speed1;
 	public float speed2;
+	public Transform gem;
 
 	void Start () {
 		agent = this.GetComponent<NavMeshAgent> ();
@@ -20,8 +21,11 @@ public class SlimeMover : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if(other.gameObject.tag == "Food") {
-			Debug.Log(other.gameObject.name + " collected!");
+			Debug.Log (other.gameObject.name + " collected!");
 			//Do not destroy food here. Add food property here. Food code block will de-activate it once collected.
+			Instantiate(gem);
+			//Im not great at coding so, can you maybe fix this? Im trying to make it so when the food is eaten spawn gem then you can collect the gem for money! dont delete the slime though.
+
 		}
 	}
 }
