b9ad76dd3f584b522b64d7ee264dcdfcaebf7c8d
JoshuaDWalters <37957669+JoshuaDWalters@users.noreply.github.com>
2018-04-03 18:25:49 +0100

    dgf
    sdfg

--- Assets/Scripts/SlimeMover.cs
+++ Assets/Scripts/SlimeMover.cs
@@ -8,7 +8,8 @@ public class SlimeMover : MonoBehaviour {
 	public Transform food;
 	public float speed1;
 	public float speed2;
-	public Transform gem;
+	public GameObject gem;
+	public Transform slime;
 
 	void Start () {
 		agent = this.GetComponent<NavMeshAgent> ();
@@ -19,11 +20,12 @@ public class SlimeMover : MonoBehaviour {
 		agent.SetDestination (food.position);	
 	}
 
-	void OnTriggerEnter(Collider other) {
+	void OnCollisionEnter(Collision other) {
 		if(other.gameObject.tag == "Food") {
 			Debug.Log (other.gameObject.name + " collected!");
 			//Do not destroy food here. Add food property here. Food code block will de-activate it once collected.
-			Instantiate(gem);
+
+			Instantiate (gem, slime.position, slime.rotation);
 			//Im not great at coding so, can you maybe fix this? Im trying to make it so when the food is eaten spawn gem then you can collect the gem for money! dont delete the slime though.
 
 		}
